a2cddaf7175a036f1773f160dd7588a0ced4c8e8
Elliott Katane <elliott.katane@gmail.com>
2023-11-17 15:18:58 -0500

    /login dans server.js wild try

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,24 @@ app.use(
   })
 );
 
+// méthode login
+app.post(
+  "/login",
+  (loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const user = await User.login(email, password);
+
+      // create a token
+      const token = createToken(user._id);
+
+      res.status(200).json({ email, token });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  })
+);
+
 // ejs
 app.set("view engine", "ejs"); // Set EJS as the view engine
 app.use(express.urlencoded({ extended: true }));

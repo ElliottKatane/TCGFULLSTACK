a45dfa3660cad1781e88127a9008c9e227a87301
Elliott Katane <elliott.katane@gmail.com>
2023-11-17 20:26:29 -0500

    update du fichier index.js qui fonctionne avec "node index.js" localement

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,17 +1,21 @@
 // express.Router() is a function that returns a router object. This object has methods like .get() and .post() that we can use to create routes. We can then export this router object and import it into our server.js file.
-const express = require("express")();
+const express = require("express");
 const app = express();
 const { v4 } = require("uuid");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+
+// middleware
+app.use(express.static(path.join(__dirname, "frontend", "src", "assets")));
+app.use(express.json());
 // routes
-const userRoutes = require("user");
-const cardRoutes = require("cardForm");
-const levelRoute = require("levelRoute");
-const monstreRoute = require(".monstreRoute");
-const levelController = require("levelController");
-const monsterController = require("monsterController");
+const userRoutes = require("./user");
+const cardRoutes = require("./cardForm");
+const levelRoute = require("./levelRoute");
+const monstreRoute = require("./monstreRoute");
+const levelController = require("../controller/levelController");
+const monsterController = require("../controller/monsterController");
 
 app.get("/api", (req, res) => {
   const path = `/api/item/${v4()}`;
@@ -25,17 +29,8 @@ app.get("/api/item/:slug", (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-// middleware
-app.use(express.static(path.join(__dirname, "frontend", "src", "assets")));
-app.use(express.json());
 require("dotenv").config({ path: "./config.env" });
-app.use(
-  cors({
-    origin: "https://tcg-front-six.vercel.app",
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 // ejs
 app.set("view engine", "ejs"); // Set EJS as the view engine
 app.use(express.urlencoded({ extended: true }));
@@ -61,7 +56,10 @@ app.get(
 );
 
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
@@ -72,8 +70,10 @@ mongoose
     console.log("MongoDB connection error", error);
   });
 
-app.on("error", (err) => {
-  console.error("Express server error:", err);
+app.use((err, req, res, next) => {
+  // Handle errors
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
 });
 // Export the Express API
 module.exports = app;

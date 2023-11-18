18a273a9591b2a3c218411d1cf4108b1cd036e7d
Elliott Katane <elliott.katane@gmail.com>
2023-11-17 19:15:12 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,25 +22,6 @@ app.use(
     credentials: true,
   })
 );
-
-// méthode login
-app.post(
-  "/login",
-  (loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    try {
-      const user = await User.login(email, password);
-
-      // create a token
-      const token = createToken(user._id);
-
-      res.status(200).json({ email, token });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
-  })
-);
-
 // ejs
 app.set("view engine", "ejs"); // Set EJS as the view engine
 app.use(express.urlencoded({ extended: true }));

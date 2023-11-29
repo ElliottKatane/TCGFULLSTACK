9b56ea92cd68ed5a1260aa22f325ed7ceeba53a9
Elliott Katane <elliott.katane@gmail.com>
2023-11-29 08:12:37 -0500

    removing useless methods (update hp monster, init hp monster)

--- frontend/src/redux/actions/monster.action.js
+++ frontend/src/redux/actions/monster.action.js
@@ -1,8 +1,6 @@
 export const FETCH_MONSTER_INFO_SUCCESS = "FETCH_MONSTER_INFO_SUCCESS";
 export const FETCH_MONSTER_INFO_FAILURE = "FETCH_MONSTER_INFO_FAILURE";
-export const UPDATE_MONSTER_HP = "UPDATE_MONSTER_HP";
 export const DEGATS_SUBIS = "DEGATS_SUBIS";
-export const INIT_HP_MONSTER = "INIT_HP_MONSTER";
 
 export const fetchMonsterInfoSuccess = (monsterInfo) => ({
   type: FETCH_MONSTER_INFO_SUCCESS,
@@ -29,20 +27,6 @@ export const fetchMonster = (mapLevel) => async (dispatch) => {
   }
 };
 
-// initialiser les points de vie du monstre
-export const initHPMonster = (health) => {
-  return {
-    type: INIT_HP_MONSTER,
-    payload: health,
-  };
-};
-
-export const updateMonsterHP = (newHP) => {
-  return {
-    type: UPDATE_MONSTER_HP,
-    payload: newHP,
-  };
-};
 // Action pour infliger les dégâts
 export const DegatsSubis = (damageValue) => {
   return {

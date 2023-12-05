ef2b3b31a9a9173b737770794e2f49e88a0737e5
ArmoMike <armomike@outlook.com>
2023-12-04 22:32:42 -0500

    d

--- frontend/src/components/StatsBar.js
+++ frontend/src/components/StatsBar.js
@@ -1,32 +1,28 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../CSS/StatsBar.css";
 
-// La valeur de la barre de HP, entre 0 et 100
 const StatsBar = ({ HPValue, currentMana, manaPool, isPlayer }) => {
   const [initialHealth, setInitialHealth] = useState(null);
 
-  // Use effect to set the initial health once when HPValue is available
   useEffect(() => {
+    console.log("StatsBar component mounted!");
     if (HPValue !== null && initialHealth === null) {
+      console.log("Setting initial health:", HPValue);
       setInitialHealth(HPValue);
     }
-  }, [HPValue, initialHealth]);
+  }, [HPValue]); // Add keyForRemount to the dependency array
 
-  // If initialHealth is not yet set, return null or loading indicator
+
   if (initialHealth === null) {
     return <p>Loading...</p>;
   }
 
-  // Calculate the percentage of HP remaining based on the constant MAXHP
   const MAXHP = initialHealth;
   const percentageRemaining = (HPValue / MAXHP) * 100;
 
-  // largeur de la partie verte (hp restants) et la largeur de la partie rouge (hp faibles)
   let greenWidth = percentageRemaining;
   let redWidth = 0;
 
-  // Si le pourcentage est < à 30, la partie verte est à 0 et la partie rouge prend la place
   if (percentageRemaining <= 30) {
     greenWidth = 0;
     redWidth = percentageRemaining;

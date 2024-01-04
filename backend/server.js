6c3f0ad53359960cf29e3adffb29b4f3cb94a6ad
ArmoMike <armomike@outlook.com>
2024-01-04 11:00:17 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,12 @@ app.use("/api/levels", levelRoute);
 app.use("/api/monsters", monsterRoute);
 app.use("/api/player", playerRoute);
 
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "public", "assets", "index.html")
+  );
+});
+
 app.get(
   "/api/levels/:mapLevel/backgroundImage",
   levelController.getLevelBackgroundImage

ad4d85e2ec641646ce86b802e8812f1a04ea9da9
ArmoMike <armomike@outlook.com>
2024-01-05 09:11:36 -0500

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import stslogo from "../assets/stslogo.png";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
 
 // We import NavLink to utilize the react router.
 import { Link, NavLink } from "react-router-dom";
+
 // Here, we display our Navbar
 export default function Navbar() {
   // hooks import
   const { logout } = useLogout();
   const { user } = useAuthContext();
 
+  // State to manage the menu visibility
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   const handleClick = () => {
     logout();
   };
+
   return (
     <header>
       <div
@@ -28,23 +37,29 @@ export default function Navbar() {
         <NavLink className="navbar-brand" to="/">
           <img src={stslogo} alt="oui"></img>
         </NavLink>
-        <Link to="/rules">Règles du jeu</Link>
 
-        <nav>
-          {/* if user is logged in, show email and logout button */}
-          {user && (
-            <div>
-              <span>{user.email}</span>
-              <button onClick={handleClick}>Log out</button>
-            </div>
-          )}
-          {!user && (
-            <div>
-              <Link to="/login">S'identifier</Link>
-              <Link to="/signup">S'enregistrer</Link>
-            </div>
-          )}
-        </nav>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <span>&#9776;</span>
+        </div>
+
+        {/* Conditional rendering based on menu visibility */}
+        {menuVisible && (
+          <nav className="menu">
+            <Link to="/rules">Règles du jeu</Link>
+
+            {user ? (
+              <div>
+                <span>{user.email}</span>
+                <button onClick={handleClick}>Log out</button>
+              </div>
+            ) : (
+              <div>
+                <Link to="/login">S'identifier</Link>
+                <Link to="/signup">S'enregistrer</Link>
+              </div>
+            )}
+          </nav>
+        )}
       </div>
     </header>
   );

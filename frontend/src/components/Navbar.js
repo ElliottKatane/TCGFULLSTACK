94821e1fbfe4d5a6ae077eccc7cf5a7652e6f227
ArmoMike <armomike@outlook.com>
2024-01-05 13:42:20 -0500

    Menu navigation

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import stslogo from "../assets/stslogo.png";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -17,37 +17,60 @@ export default function Navbar() {
 
   const handleClick = () => {
     logout();
+    setMenuVisible(false); // Close the menu after logout
   };
 
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      // Close the menu if clicked outside of it
+      if (menuVisible && !event.target.closest(".menu-container")) {
+        setMenuVisible(false);
+      }
+    };
+
+    // Add event listener when the component mounts
+    document.addEventListener("click", handleOutsideClick);
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, [menuVisible]);
+
   return (
-    <header>
-      <div className="container">
-        <div className="navbar">
-          <div className="logo">
-            <img src={stslogo} alt="oui"></img>
-          </div>
-
-          <div className="menu-icon" onClick={toggleMenu}>
-            <span>&#9776;</span>
-          </div>
-
-          <nav className={`menu ${menuVisible ? "open" : ""}`}>
-            <Link to="/rules">Règles du jeu</Link>
-
-            {user ? (
-              <div>
-                <span>{user.email}</span>
-                <button onClick={handleClick}>Log out</button>
-              </div>
-            ) : (
-              <div>
-                <Link to="/login">S'identifier</Link>
-                <Link to="/signup">S'enregistrer</Link>
-              </div>
-            )}
-          </nav>
-        </div>
+    <div className="menu-container">
+      <div className="menu-icon" onClick={toggleMenu}>
+        <span>&#9776;</span>
       </div>
-    </header>
+
+      <nav className={`menu ${menuVisible ? "open" : ""}`}>
+        <Link to="/map" onClick={() => setMenuVisible(false)}>
+          <img src={stslogo} alt="oui" className="logo-img" />
+        </Link>
+        {user ? (
+          <>
+            <div className="UserEmail">{user.email}</div>
+            <div>
+              <Link to="/rules" onClick={() => setMenuVisible(false)}>
+                Règles du jeu
+              </Link>
+            </div>
+            <div>
+              <button onClick={handleClick}>Déconnexion</button>
+            </div>
+          </>
+        ) : (
+          <div>
+            <Link to="/login" onClick={() => setMenuVisible(false)}>
+              S'identifier
+            </Link>
+            <br />
+            <Link to="/signup" onClick={() => setMenuVisible(false)}>
+              S'enregistrer
+            </Link>
+          </div>
+        )}
+      </nav>
+    </div>
   );
 }
